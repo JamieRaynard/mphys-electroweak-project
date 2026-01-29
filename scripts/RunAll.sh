ccf6e589cfbfd8cd253b91f177d4396f4b710183
Luc Thomas <phuwdj@bonette.epp.warwick.ac.uk>
2026-01-29 14:49:31 +0000

    table success

--- scripts/RunAll.sh
+++ scripts/RunAll.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-./run ./scripts/Upsilon.py --FullOutput=TRUE
-./run ./scripts/Z_Simaltaneous_fits.py --Calibration=TRUE --Smear_error=PLUS 
-./run ./scripts/Z_Simaltaneous_fits.py --Calibration=TRUE --Smear_error=MINUS 
-./run ./scripts/Z_Simaltaneous_fits.py --Calibration=TRUE --C_ratio=PLUS 
-./run ./scripts/Z_Simaltaneous_fits.py --Calibration=TRUE --C_ratio=MINUS 
-./run ./scripts/Z_mass_width_calibration_er
+./run ./scripts/Upsilon.py --FullOutput="TRUE"
+./run ./scripts/Upsilon.py --Compare="TRUE"
+./run ./scripts/Z_Simaltaneous_fits.py --Calibration="TRUE" --Smear_error=PLUS 
+./run ./scripts/Z_Simaltaneous_fits.py --Calibration="TRUE" --Smear_error=MINUS 
+./run ./scripts/Z_Simaltaneous_fits.py --Calibration="TRUE" --C_ratio=PLUS 
+./run ./scripts/Z_Simaltaneous_fits.py --Calibration="TRUE" --C_ratio=MINUS 
+./run ./scripts/Z_mass_width_calibration_error.py
 ./run pdflatex Images.tex
