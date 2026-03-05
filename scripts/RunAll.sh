052b9901d0bf3ba473e28434b2550646e131c87e
Jraynard <jamie.raynard@warwick.ac.uk>
2026-03-05 14:44:25 +0000

    Small amount of parallelisation and stops repeated uses of ./run

--- scripts/RunAll.sh
+++ scripts/RunAll.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
-./run ./scripts/Upsilon.py --FullOutput="TRUE"
-./run ./scripts/Upsilon.py --Compare="TRUE"
-./run ./scripts/Z_Simaltaneous_fits.py --Calibration="TRUE" --Smear_error=PLUS 
-./run ./scripts/Z_Simaltaneous_fits.py --Calibration="TRUE" --Smear_error=MINUS 
-./run ./scripts/Z_Simaltaneous_fits.py --Calibration="TRUE" --C_ratio=PLUS 
-./run ./scripts/Z_Simaltaneous_fits.py --Calibration="TRUE" --C_ratio=MINUS 
+./scripts/Upsilon.py --FullOutput="TRUE"
+./scripts/Upsilon.py --Compare="TRUE"
 
-./run ./scripts/Z_Simaltaneous_fits.py --Calibration="TRUE" 
-./run ./scripts/Z_mass_width_calibration_error.py
-./run pdflatex Images.tex
+./scripts/Z_Simaltaneous_fits.py --Calibration="TRUE" --Smear_error=PLUS &
+./scripts/Z_Simaltaneous_fits.py --Calibration="TRUE" --Smear_error=MINUS &
+./scripts/Z_Simaltaneous_fits.py --Calibration="TRUE" --C_ratio=PLUS &
+./scripts/Z_Simaltaneous_fits.py --Calibration="TRUE" --C_ratio=MINUS &
+./scripts/Z_Simaltaneous_fits.py --Calibration="TRUE" &
+wait
+
+./scripts/Z_mass_width_calibration_error.py
+pdflatex Images.tex
